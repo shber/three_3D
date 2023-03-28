20c9dad235d4f686b10e666e63c7c3421183b1f9
Shber <web648@126.com>
2023-03-28 18:01:51 +0800

    室内全景

--- src/views/house/index.vue
+++ src/views/house/index.vue
@@ -2,16 +2,20 @@
  * @Author: Shber
  * @Date: 2023-03-25 14:44:04
  * @LastEditors: Shber
- * @LastEditTime: 2023-03-28 17:34:27
+ * @LastEditTime: 2023-03-28 18:01:39
  * @Description: 
 -->
 <template>
   <div class="house" id="house">
     <div class="controls">
-      <span @click="checkScene('house')">主卧</span>
-      <span @click="checkScene('street')">街道</span>
-      <span>洗手间</span>
-      <span>厨房</span>
+      <span class="controls_btn" @click="checkScene('house')">主卧</span>
+      <span class="controls_btn" @click="checkScene('street')">街道</span>
+      <span class="controls_btn">洗手间</span>
+      <span class="controls_btn">厨房</span>
+    </div>
+    <div class="top_controls">
+      <span class="controls_btn" @click="fullScreen">全屏</span>
+      <span class="controls_btn">暂停</span>
     </div>
   </div>
 </template>
@@ -40,6 +44,19 @@
     initMesh() // 创建网格模型
     initControls() // 添加控制器
     // initGui() // 
+
+    // onresize 事件会在窗口被调整大小时发生
+    window.onresize = function () {
+        // 重置渲染器输出画布canvas尺寸
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        // 全屏情况下：设置观察范围长宽比aspect为窗口宽高比
+        camera.aspect = window.innerWidth / window.innerHeight;
+        // 渲染器执行render方法的时候会读取相机对象的投影矩阵属性projectionMatrix
+        // 但是不会每渲染一帧，就通过相机的属性计算投影矩阵(节约计算资源)
+        // 如果相机的一些属性发生了变化，需要执行updateProjectionMatrix ()方法更新相机的投影矩阵
+        camera.updateProjectionMatrix();
+    };
+
   })
 
 
@@ -74,13 +91,21 @@
   let gui = reactive({}) // 全局控制
 
 
-  const checkScene = ()=>{
-    modelName.value = 'street'
-    // removeScene()
-    // initModel()
-    // initMesh()
+  const checkScene = (_name)=>{
+    if(modelName.value === _name){return false}
+    modelName.value = _name
+    materials = []
+    removeScene()
+    setTimeout(()=>{
+      initModel()
+      initMesh()
+    },300)
+
   }
 
+  const fullScreen = ()=>{
+    document.documentElement.webkitRequestFullScreen();
+  }
 
   const initScene = () =>{ // 创建3D场景
     scene = new THREE.Scene();
@@ -163,7 +188,8 @@
   left: 50%;
   transform: translateX(-50%);
   z-index: 9;
-  span{
+}
+span.controls_btn{
     font-weight: 500;
     margin: 0 10px;
     background: rgba($color: #fff, $alpha: 0.6);
@@ -171,5 +197,7 @@
     border-radius: 8px;
     cursor: pointer;
   }
+.top_controls{
+  position: absolute; top: 20px; right: 20px; z-index: 8;
 }
 </style>
